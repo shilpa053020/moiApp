1bf381923f47f1459088bdbca31a1c3911873732
kaviArasan09 <kk8689149@gmail.com>
2023-06-13 02:57:36 -0400

    login

--- frontent/src/UserLogin/userlogin.jsx
+++ frontent/src/UserLogin/userlogin.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import "./userlogin.css"
 
-const Userlogin = () => {
-    const navigate = useNavigate();
-    const dashboardnav = ()=>{
-        navigate("/dashboard");
-    }
+
+function Login() {
 
 
+  return (
 
- return ( 
-        <div>
-            <h1>LOGIN</h1>
-           
-        </div>
-            
-       
-        
-)
+    <div className='box'>
+      <div className='container'>
+        <h1>Login </h1>
+      </div>
+
+      <div className='input-field'>
+        <input type='text' className='input' placeholder='Username' />
+      </div>
+      <br></br>
+      <div className='input-field'>
+        <input type='password' className='input' placeholder='Password' />
+      </div>
+      <br></br>
+      <div className='input-field'>
+        <input type='submit' className='submit' value="Login" />
+      </div>
+    </div>
+  )
 }
-export default Userlogin;
+
+export default Login
